2d0934d5e45053ddb245d9b6d8a1fe87c9f4743e
Lucas Tadeu <30877738+lucastg@users.noreply.github.com>
2019-05-15 20:10:32 -0300

    Add files via upload

---
+++ Servidor/src/servidor.java
@@ -0,0 +1,24 @@
+import java.io.IOException;
+import java.net.*;
+import java.util.Scanner;
+
+public class servidor {
+	public static void main(String[] args) throws IOException {
+        ServerSocket servidor = new ServerSocket(12345);
+        System.out.println("Porta 12345 aberta!");
+
+        Socket cliente = servidor.accept();
+        System.out.println("Nova conexão com o cliente " +     
+            cliente.getInetAddress().getHostAddress()
+        );
+
+        Scanner s = new Scanner(cliente.getInputStream());
+        while (s.hasNextLine()) {
+            System.out.println(s.nextLine());
+        }
+
+        s.close();
+        servidor.close();
+        cliente.close();
+    }
+}
